45be6dde1432b5a486d18ace040ee2005ffdc25d
Collen Maphike <collenmaphike25@gmail.com>
2020-08-21 12:05:16 +0200

    cart component

---
+++ spaza/src/containers/Cart.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+
+const Cart = () => {
+  const cart = useSelector(state => state.cart)
+  console.log("cart", cart);
+  return (<div className="Items">
+    <h1>Cart</h1>
+    {cart.map(item =>
+      <div className="Item" key={item.id}>
+        <label>{item.name}: R{item.price}</label>
+      </div>
+    )}
+  </div>)
+}
+
+
+export default Cart;
--- spaza/src/containers/Shopping.jsx
+++ spaza/src/containers/Shopping.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ADD_TO_CART } from '../redux/actions/actionTypes';
+import Cart from './Cart';
 
 
 const Shopping = () => {
@@ -10,52 +11,59 @@ const Shopping = () => {
   const [advocados, setAdvocados] = useState({ name: "advocados", id: 3, price: 28 });
   const [grapes, setGrapes] = useState({ name: "grapes", id: 4, price: 25 });
   const [lemons, setLemons] = useState({ name: "lemons", id: 5, price: 15 });
-  const cart = useSelector(state => state.cart);
+  const [toggleCart, setToggleCart] = useState({ state: false })
   const dispatch = useDispatch()
 
-
+  const showCart = () => {
+    setToggleCart({
+      state: !toggleCart.state
+    })
+    console.log(toggleCart.state)
+  }
 
   return (<div>
+
     <header>
       <h2 className="Online">TCG Online Shopping</h2>
-      <button>Cart</button>
+      <button className="Cart-button" onClick={showCart}>Cart</button>
     </header>
-    <div className="Products">
-      <div className="Fruits">
-
-        <h3>Fruits</h3>
-        <div className="Apples">
-          <img src="https://cdn.pixabay.com/photo/2018/08/02/21/51/red-3580560_960_720.jpg" alt="fresh apples" />
-          <br />
-          <label>R22 p/k</label>
-          <button onClick={() => dispatch({ type: ADD_TO_CART, payload: apples })}>add to cart</button>
-        </div>
-        <div className="Bannas">
-          <img src="https://cdn.pixabay.com/photo/2015/03/30/12/43/bananas-698608_960_720.jpg" alt="fresh bananas" />
-          <br />
-          <label>R18 p/k</label>
-          <button onClick={() => dispatch({ type: ADD_TO_CART, payload: bananas })}>add to cart</button>
-        </div>
-        <div className="Advocados">
-          <img src="https://cdn.pixabay.com/photo/2017/05/28/14/46/avocado-2351191_960_720.jpg" alt="fresh advocados" />
-          <br />
-          <label>R28 p/k</label>
-          <button onClick={() => dispatch({ type: ADD_TO_CART, payload: advocados })}>add to cart</button>
-        </div>
-        <div className="Grapes">
-          <img src="https://cdn.pixabay.com/photo/2019/09/22/12/37/grapes-4495944_960_720.jpg" alt="fresh grapes" />
-          <br />
-          <label>R25 p/k</label>
-          <button onClick={() => dispatch({ type: ADD_TO_CART, payload: grapes })}>add to cart</button>
-        </div>
-        <div className="Lemons">
-          <img src="https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830_960_720.jpg" alt="fresh lemons" />
-          <br />
-          <label>R25 p/k</label>
-          <button onClick={() => dispatch({ type: ADD_TO_CART, payload: grapes })}>add to cart</button>
+    {toggleCart.state == false ?
+      <div className="Products">
+        <div className="Fruits">
+          <h3>Fruits</h3>
+          <div className="Apples">
+            <img src="https://cdn.pixabay.com/photo/2018/08/02/21/51/red-3580560_960_720.jpg" alt="fresh apples" />
+            <br />
+            <label>R22 p/k</label>
+            <button onClick={() => dispatch({ type: ADD_TO_CART, payload: apples })}>add to cart</button>
+          </div>
+          <div className="Bannas">
+            <img src="https://cdn.pixabay.com/photo/2015/03/30/12/43/bananas-698608_960_720.jpg" alt="fresh bananas" />
+            <br />
+            <label>R18 p/k</label>
+            <button onClick={() => dispatch({ type: ADD_TO_CART, payload: bananas })}>add to cart</button>
+          </div>
+          <div className="Advocados">
+            <img src="https://cdn.pixabay.com/photo/2017/05/28/14/46/avocado-2351191_960_720.jpg" alt="fresh advocados" />
+            <br />
+            <label>R28 p/k</label>
+            <button onClick={() => dispatch({ type: ADD_TO_CART, payload: advocados })}>add to cart</button>
+          </div>
+          <div className="Grapes">
+            <img src="https://cdn.pixabay.com/photo/2019/09/22/12/37/grapes-4495944_960_720.jpg" alt="fresh grapes" />
+            <br />
+            <label>R25 p/k</label>
+            <button onClick={() => dispatch({ type: ADD_TO_CART, payload: grapes })}>add to cart</button>
+          </div>
+          <div className="Lemons">
+            <img src="https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830_960_720.jpg" alt="fresh lemons" />
+            <br />
+            <label>R25 p/k</label>
+            <button onClick={() => dispatch({ type: ADD_TO_CART, payload: grapes })}>add to cart</button>
+          </div>
         </div>
       </div>
-    </div>
+      : <Cart />}
   </div>)
 }
 
